d99f601418971e35c3fc8488393754653b59524b
DESKTOP-DAV8INI\USER <jueunyu72@gmail.com>
2023-08-30 18:08:29 +0900

    프로젝트 시작 : 기본 메인 브랜치용

---
+++ src/main/java/edu/global/yuflix/SpringBootYjeHello2Application.java
@@ -0,0 +1,13 @@
+package edu.global.yuflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootYjeHello2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootYjeHello2Application.class, args);
+	}
+
+}
---
+++ src/main/java/edu/global/yuflix/controller/HomeController.java
@@ -0,0 +1,13 @@
+package edu.global.yuflix.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "안녕하세요";
+	}
+}
